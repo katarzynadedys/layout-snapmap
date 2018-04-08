2b3223bb14c51d37b365b75a7251bb1dd64de730
katarzynadedys <34009517+katarzynadedys@users.noreply.github.com>
2018-04-08 19:37:02 +0200

    SnapMap

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".about-nav").click(function(){
+    $("body, html").animate({scrollTop: 220}, 1300);
+});
+
+$(".faq-nav").click(function(){
+    $("body, html").animate({scrollTop: 955}, 1300);
+});
+
+$(".support-nav").click(function lol(){
+    $("body, html").animate({scrollTop: 1630}, 1300);
+});
+
+$(".download-nav").click(function(){
+    $("body, html").animate({scrollTop: 3100}, 1300);
+});
+
